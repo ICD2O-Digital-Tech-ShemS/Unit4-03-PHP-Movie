b878af58023faaf1d8cfe964bdfcdc214da57fb6
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-05 15:14:06 +0000

    Update 5 Assign-03

--- result.php
+++ result.php
@@ -4,14 +4,14 @@
             $Final = "You can watch PG18 Moves";
         }
         else if (WatchMove >= 13) {
-           $Final = "You are only allowed to watch PG13"; 
+            echo  "You are only allowed to watch PG13"; 
     }
     else if (WatchMove <= 12) {
-       $Final = "You are only allowed to watch PG"; 
+        echo "You are only allowed to watch PG"; 
     }
     else {
-        $Final = "Enter a Valid Age"; 
+        echo "Enter a Valid Age"; 
     }
 ?>
 <h3>Results:</h3>
-    <?php echo "$Final" ?>°F.
+    <?php echo "$Final" ?>.
